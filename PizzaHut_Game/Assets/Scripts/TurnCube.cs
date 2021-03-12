3362c65f35b572d609fd2d5a6d8b5d68338e5da6
Brandon Watson <bmwtsn99@gmail.com>
2021-03-12 00:39:38 -0700

    Actually added game scene this time

---
+++ PizzaHut_Game/Assets/Scripts/TurnCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnCube : MonoBehaviour {
+
+    public Color first = Color.yellow;
+    public Color second = Color.cyan;
+    private float speed = 1;
+    private float duration = 1;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate((float).6, (float).6, (float).6);
+
+        float lerp = Mathf.PingPong(Time.time * speed, duration);
+
+        rend.material.color = Color.Lerp(first, second, lerp);
+    }
+}
