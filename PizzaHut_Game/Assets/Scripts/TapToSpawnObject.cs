f684391457faf57c575ec87f3966f86d38b695f6
Brandon Watson <bmwtsn99@gmail.com>
2021-03-16 00:22:06 -0700

    Fixed object spawn location

--- PizzaHut_Game/Assets/Scripts/TapToSpawnObject.cs
+++ PizzaHut_Game/Assets/Scripts/TapToSpawnObject.cs
@@ -2,34 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
+[RequireComponent(typeof(ARRaycastManager))]
 public class TapToSpawnObject : MonoBehaviour
 {
 
     public GameObject AR_object;
-    public Camera AR_Camera;
-    public ARRaycastManager raycastManager;
-    public List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private GameObject spawnedObject;
+    private ARRaycastManager raycastManager;
+    private Vector2 touchPos;
+
+    static public List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool getTouch(out Vector2 touchPos)
+    {
+
+        if(Input.touchCount > 0)
+        {
+
+            touchPos = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPos = default;
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (!getTouch(out Vector2 touchPos))
+            return;
+
+        if(raycastManager.Raycast(touchPos, hits, TrackableType.PlaneWithinPolygon))
         {
-            Ray ray = AR_Camera.ScreenPointToRay(Input.mousePosition);
-            if (raycastManager.Raycast(ray, hits))
+
+            var hitPose = hits[0].pose;
+
+            if(spawnedObject = null)
             {
-                Pose pose = hits[0].pose;
-                Instantiate(AR_object, pose.position, pose.rotation);
+
+                spawnedObject = Instantiate(AR_object, hitPose.position, hitPose.rotation);
+            } else
+            {
+
+                AR_object.transform.position = hitPose.position;
             }
         }
-
     }
 }
