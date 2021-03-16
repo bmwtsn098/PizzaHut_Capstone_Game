4467d4f86811a17076b7e981830856c1659296ea
Brandon Watson <bmwtsn99@gmail.com>
2021-03-16 12:26:22 -0700

    trying to fix spawning again

--- PizzaHut_Game/Assets/Scripts/TapToSpawnObject.cs
+++ PizzaHut_Game/Assets/Scripts/TapToSpawnObject.cs
@@ -7,7 +7,7 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycastManager))]
 public class TapToSpawnObject : MonoBehaviour
 {
-
+    /*
     public GameObject gameObjectToTnstantiate;
 
     private GameObject spawnedObject = null;
@@ -56,6 +56,63 @@ public class TapToSpawnObject : MonoBehaviour
             {
 
                 gameObjectToTnstantiate.transform.position = hitPose.position;
+            }
+        }
+    }
+    */
+
+    private ARRaycastManager raycastManager;
+    private GameObject spawnedObject;
+
+    [SerializeField]
+    private GameObject PlaceablePrefab;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+
+        if(Input.touchCount > 0)
+        {
+
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    private void Update()
+    {
+
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+
+            return;
+        }
+
+        if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+
+            var hitPose = s_Hits[0].pose;
+
+            if(spawnedObject == null)
+            {
+
+                spawnedObject = Instantiate(PlaceablePrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+
+                spawnedObject.transform.position = hitPose.position;
+                spawnedObject.transform.rotation = hitPose.rotation;
             }
         }
     }
