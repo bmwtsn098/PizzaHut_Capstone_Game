ffdee2d8af98f11ec5218ccf2574ef321b222693
Brandon Watson <bmwtsn99@gmail.com>
2021-03-30 11:25:07 -0700

    Revert "Revert "First attempt at level placement and player control""
    This reverts commit 2bcf9c82baebf5bfed6ac3c87a1c104215720219.

--- PizzaHut_Game/Assets/Scripts/TapToSpawnObject.cs
+++ PizzaHut_Game/Assets/Scripts/TapToSpawnObject.cs
@@ -63,16 +63,33 @@ public class TapToSpawnObject : MonoBehaviour
 
     private ARRaycastManager raycastManager;
     private GameObject spawnedObject;
+    private GameObject Level;
 
     [SerializeField]
     private GameObject PlaceablePrefab;
+    [SerializeField]
+    private GameObject Obstacle;
 
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    public GameObject joystick;
+    public GameObject button;
+    public GameObject text;
+
+    private bool firstTouch = true;
+    private bool obstacle = false;
+
+    private List<GameObject> ObstacleList = new List<GameObject>();
+    public int maxObstacles = 0;
+    private int curObstacleCount = 0;
 
     private void Awake()
     {
 
         raycastManager = GetComponent<ARRaycastManager>();
+        joystick.SetActive(false);
+        button.SetActive(false);
+        text.SetActive(true);
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
@@ -101,19 +118,61 @@ public class TapToSpawnObject : MonoBehaviour
         if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
         {
 
-            var hitPose = s_Hits[0].pose;
-
-            if(spawnedObject == null)
+            if (firstTouch)
             {
 
-                spawnedObject = Instantiate(PlaceablePrefab, hitPose.position, hitPose.rotation);
+                var fhitPose = s_Hits[0].pose;
+                Level = Instantiate(PlaceablePrefab, fhitPose.position, fhitPose.rotation);
+
+                while (Input.GetTouch(0).phase != TouchPhase.Ended)
+                {
+
+                    Level.transform.position = fhitPose.position;
+                    Level.transform.rotation = fhitPose.rotation;
+                }
+
+                firstTouch = false;
+                text.SetActive(false);
+                joystick.SetActive(true);
+                button.SetActive(true);
             }
             else
             {
 
-                spawnedObject.transform.position = hitPose.position;
-                spawnedObject.transform.rotation = hitPose.rotation;
-            }
+                var hitPose = s_Hits[0].pose;
+
+                if (curObstacleCount < maxObstacles && obstacle)
+                {
+
+                    spawnedObject = Instantiate(PlaceablePrefab, hitPose.position, hitPose.rotation);
+                    while (Input.GetTouch(0).phase != TouchPhase.Ended)
+                    {
+
+                        spawnedObject.transform.position = hitPose.position;
+                        spawnedObject.transform.rotation = hitPose.rotation;
+                    }
+
+                    ObstacleList.Add(spawnedObject);
+
+                }
+            } 
         }
+    }
+
+    public void placeCubes()
+    {
+        if(obstacle)
+        {
+
+            obstacle = false;
+            joystick.SetActive(true);
+        }
+        else
+        {
+
+            obstacle = true;
+            joystick.SetActive(false);
+        }
+        
     }
 }
