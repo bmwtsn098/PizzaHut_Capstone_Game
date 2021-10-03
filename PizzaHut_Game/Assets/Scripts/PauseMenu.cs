b98bd324ac47a9938ec6e527f064fdfc4eddca9d
Brandon Watson <bmwtsn99@gmail.com>
2021-10-02 19:07:38 -0700

    Added Simple Pause Menu

---
+++ PizzaHut_Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool paused = false;
+    public GameObject pauseMenu;
+
+    public void Pause()
+    {
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Resume()
+    {
+
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
