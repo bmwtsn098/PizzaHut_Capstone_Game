eb2d9df4b69a2d9a677b9cd189da7fa099961557
Brandon Watson <bmwtsn99@gmail.com>
2021-11-08 13:54:17 -0700

    Added Quick Level Select Menu

---
+++ PizzaHut_Game/Assets/Scripts/Level_Select.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Select : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Level1()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level2()
+    {
+
+        SceneManager.LoadScene(3);
+    }
+
+}
--- PizzaHut_Game/Assets/Scripts/PlayerJoystick.cs
+++ PizzaHut_Game/Assets/Scripts/PlayerJoystick.cs
@@ -54,7 +54,7 @@ public class PlayerJoystick : MonoBehaviour
 		if(collision.gameObject.CompareTag("Exit"))
         {
 
-			SceneManager.LoadScene(2);
+			SceneManager.LoadScene(4);
         }
 
 		if(collision.gameObject.CompareTag("DeliveryLocation"))
